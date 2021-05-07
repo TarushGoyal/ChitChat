b8854d0ee9c6b50f1fae867a85f8e5a818c2dd81
Tarush Goyal <tarushgoyal2000@gmail.com>
2021-05-07 17:22:53 +0530

    reply finally done

--- events.py
+++ events.py
@@ -13,7 +13,12 @@ def chat(json, methods=['GET', 'POST']):
         return
     json['user_name'] = current_user.name
     room = json['channel']
-    msg = Message(content = json['message'], type = "text", posted_in = room, posted_by = current_user.id)
+    reply_id = json['reply_id']
+    print(json)
+    if not reply_id:
+        msg = Message(content = json['message'], type = "text", posted_in = room, posted_by = current_user.id)
+    else:
+        msg = Message(content = json['message'], type = "text", posted_in = room, posted_by = current_user.id, reply_to = reply_id)
     db.session.add(msg)
     db.session.commit()
     json['id'] = msg.id
@@ -22,6 +27,7 @@ def chat(json, methods=['GET', 'POST']):
     del msg_json['_sa_instance_state']
     msg_json['posted_by_name'] = User.query.get(msg_json['posted_by']).name
     msg_json['posted_at'] = str(msg_json['posted_at'])
+    print(msg_json)
     emit('add chat', msg_json, room = room)
 
 @socketIO.on('upload', namespace = "/")
@@ -70,9 +76,19 @@ def reacted(json):
     message_id = json['message_id']
     type = json['type']
     if type not in ['love', 'laugh', 'like', 'angry', 'wow', 'sad']:
-        assert(False)
+        return
     react = React(react_type = type, reacted_to = message_id, reacted_by = current_user.id)
     db.session.add(react)
     db.session.commit()
     json['id'] = react.id;
     emit('add react' , json, room = room)
+
+@socketIO.on('deleted', namespace='/')
+@login_required
+def reacted(json):
+    room = json['channel']
+    message_id = json['message_id']
+    message = Message.query.get(message_id)
+    message.deleted = 1;
+    db.session.commit()
+    emit('delete chat' , json, room = room)
--- static/js/channel.js
+++ static/js/channel.js
@@ -11,6 +11,17 @@ var socket = io.connect('http://' + document.domain + ':' + location.port);
 
 socket.on( 'connect', function() {
 
+  function get_reply_holder(id){
+    var reply_sender = $('#complete-message-'+id).find('.sender').first().text();
+    var reply_content = $('#complete-message-'+id).find('.message-content').last().text();
+    var reply_time = $('#complete-message-'+id).find('.timestamp').first().text();
+    return $('<div class="reply-holder"> \
+              <div class="sender">' + reply_sender + '</div> \
+              <div class="timestamp">' + reply_time + '</div> \
+              <div class="message-content">' + reply_content + '</div> \
+              </div>');
+  }
+
   function onReacted(id, key) {
       return function() {
           socket.emit('reacted', {
@@ -21,16 +32,47 @@ socket.on( 'connect', function() {
       };
   }
 
-  function add_message(id, sender_name, time, text, reactions, type) {
+  function onDeleted(id) {
+      return function() {
+          socket.emit('deleted', {
+              channel : channel_id,
+              message_id : id,
+          });
+      };
+  }
+
+  function onReplied(id) {
+      return function() {
+        var curr = $('.text-form .reply_id').val();
+        if (curr != "" && curr != undefined){
+              $('.form-holder .reply-holder').remove();
+        }
+        if (curr != id){
+            $('.form-holder').prepend(get_reply_holder(id));
+            $('.text-form .reply_id').val(id);
+        }
+        else{
+            $('.text-form .reply_id').val(undefined);
+        }
+    };
+  }
+
+  function add_message(id, sender_name, time, text, reactions, type, deleted, replied) {
       // console.log("Adding message : ", type, id, sender_name, time, text, reactions);
       var sender = $('<div class="sender">' + sender_name + '</div>');
       var time = $('<div class="timestamp">' + time + '</div>');
       var message_content;
-      if (type == "text")
+      if (deleted == 1)
+          message_content = $('<div class="message-content"> <i>This message has been deleted </i> </div>');
+      else if (type == "text")
           message_content = $('<div class="message-content">' + text + '</div>');
       else { // file
           var filename = text.replace(/^.*[\\\/]/, '')
           message_content = $('<div class="message-content"><a href="' + text + '" target="_blank" > <div class="filediv"> 📰️ ' + filename + ' </div> </a></div>');
+      }
+      var reply_holder;
+      if (replied) {
+          reply_holder =  get_reply_holder(replied);
       }
       var react_state = $('<div class="react-state"></div>');
       for(var key in REACT2EMOJI) {
@@ -51,18 +93,27 @@ socket.on( 'connect', function() {
           bt.click(onReacted(id,key));
           react_button_tray.append(bt);
       }
+
       var reply_button = $('<button class="reply-button" id="reply-button'+id+'">' + "💬️" + '</button>');
+      reply_button.click(onReplied(id));
       react_button_tray.append(reply_button);
+
       var delete_button = $('<button class="delete-button" id="delete-button'+id+'">' + "️🚫️" + '</button>');
+      delete_button.click(onDeleted(id));
       react_button_tray.append(delete_button);
-      var complete_message = $('<div class="complete-message"></div>');
-      complete_message.append(sender, time, message_content, react_state, react_button_tray, $('<div style="clear:both;"></div>'));
-      // if (reply_to) {
-      //   complete_message.append($('<p>replied message</p>'))
-      // }
+
+      var complete_message = $('<div id="complete-message-' + id + '" class="complete-message"></div>');
+      if (deleted)
+          complete_message.append(sender, time, message_content);
+      else if (replied)
+          complete_message.append(sender, time, reply_holder, message_content, react_state, react_button_tray, $('<div style="clear:both;"></div>'));
+      else
+          complete_message.append(sender, time, message_content, react_state, react_button_tray, $('<div style="clear:both;"></div>'));
       complete_message.mouseenter(() => {react_button_tray.css("display", "inline");});
       complete_message.mouseleave(() => {react_button_tray.css("display", "none");});
+      // $('.message_holder').remove($('#message-spacer'));
       $('.message_holder').append(complete_message);
+      // $('.message_holder').append($('#message-spacer'));
 
   }
 
@@ -80,7 +131,6 @@ socket.on( 'connect', function() {
             for (var i = 0; i < msg.chats.length; i++) {
               var chat = msg.chats[i];
               var samaan = (chat.type == "text") ? chat.content : chat.link;
-              console.log(chat.type);
               add_message(chat.id, chat.name, chat.posted_at, samaan, {
                  like  : chat.like,
                  love  : chat.love,
@@ -88,7 +138,7 @@ socket.on( 'connect', function() {
                  laugh : chat.laugh,
                  wow   : chat.wow,
                  sad   : chat.sad,
-              }, chat.type);
+              }, chat.type, chat.deleted,chat.reply_to);
             }
             document.querySelector('.rightcolumn').scrollTop = document.querySelector('.rightcolumn').scrollHeight;
         },
@@ -107,11 +157,15 @@ socket.on( 'connect', function() {
 
   var form = $( '.text-form' ).on( 'submit', function( e ) {
     e.preventDefault();
+    let reply_id = $( 'input.reply_id' ).val();
     let user_input = $( 'input.message' ).val();
+    $('.form-holder .reply-holder').remove();
     socket.emit( 'send chat', {
       message : user_input,
-      channel : channel_id
+      channel : channel_id,
+      reply_id : reply_id
     } );
+    $( 'input.reply_id' ).val(undefined);
     console.log("Message sent from frontend");
     $( 'input.message' ).val( '' ).focus();
   } );
@@ -187,7 +241,7 @@ socket.on('add chat',function(msg) {
           laugh : 0,
           wow   : 0,
           sad   : 0,
-        }, "text");
+        }, "text", msg.deleted, msg.reply_to);
     }
     else if (msg.type == "file"){
       add_message(msg.id, msg.posted_by_name, msg.posted_at, msg.link, {
@@ -197,7 +251,7 @@ socket.on('add chat',function(msg) {
           laugh : 0,
           wow   : 0,
           sad   : 0,
-        }, "file");
+        }, "file", msg.deleted, msg.reply_to);
     }
     else {
       console.log("Fucked up! " + msg.type);
@@ -213,6 +267,13 @@ socket.on('add react',function(msg) {
         react_num.style.display = "inline";
         react_type.style.display = "inline";
     }
+})
+
+socket.on('delete chat',function(msg) {
+    $('#complete-message-'+msg.message_id).find('.react-button-tray').remove();
+    $('#complete-message-'+msg.message_id).find('.react-state').remove();
+    $('#complete-message-'+msg.message_id).find('.reply-holder').remove();
+    $('#complete-message-'+msg.message_id).find('.message-content').html('<i>This message has been deleted </i>');
 })
 
 } );
